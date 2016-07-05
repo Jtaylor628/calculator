27521d974af707b150f617e069c7f2e4b92ac142
John Taylor <taylorjohn628@outlook.com>
2016-07-05 19:07:21 -0400

    changed size of button

--- junk/Form1.Designer.cs
+++ junk/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(70, 129);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 144);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
